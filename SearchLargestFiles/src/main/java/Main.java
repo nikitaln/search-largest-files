e70648b62bbd0d3e1b7ae054e58608174e1eba6f
nikita_lukanin <nikitaln7@gmail.com>
2023-03-07 21:56:03 +0300

    method search file added

--- SearchLargestFiles/src/main/java/Main.java
+++ SearchLargestFiles/src/main/java/Main.java
@@ -1,6 +1,28 @@
+import java.io.File;
+import java.util.List;
+
 public class Main {
+
+    private static String path = "C:\\nikita_folder\\test";
     public static void main(String[] args) {
 
         System.out.println("Программа для поиска самых больших файлов. by nikita_ln");
+
+        File file = new File(path);
+
+        if (!file.isDirectory()) {
+            System.out.println("Папка не сушествует");
+        } else {
+
+            File[] dirFiles = file.listFiles();
+
+            for (int i = 0; i < dirFiles.length; i++) {
+
+                System.out.println(dirFiles[i].length());
+            }
+
+        }
+
+
     }
 }
